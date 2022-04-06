3f657e627c520bd10ce3bea34263b768aa930b7c
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-04-06 15:19:54 +0100

    initialisation du backend => package JSON, express, nodemon, variables env

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,7 @@
-console.log('test');
+//importation du framwork express
+const express = require('express');
+//création de l'application express
+const app = express();
+
+// exportation application express pour les autres modules
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-console.log('test');
+//importer le package HTTP de Node.js pour avoir les outils pour créer le serveur
+const http = require('http');
+// importation de l'application app.js pour gérer les requêtes envoyées vers le serveur
+const app = require('./app');
+//importer le package pour utiliser les variables d'environnement
+require('dotenv').config();
