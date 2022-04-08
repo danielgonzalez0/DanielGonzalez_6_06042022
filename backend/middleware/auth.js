043ac869d8a196cd0788121e39eb892091f2d1e1
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-04-08 11:43:52 +0100

    ajout middleware d'authentification

---
+++ backend/middleware/auth.js
@@ -0,0 +1,35 @@
+//config middleware d'authentification pour protéger les routes
+
+//====================================================================
+//                      importation Package
+
+//importation jsonwebtoken
+const jwt = require('jsonwebtoken');
+
+//importer le package pour utiliser les variables d'environnement
+require('dotenv').config();
+
+//====================================================================
+//                      structure du middleware
+
+module.exports = (req, res, next) => {
+  try {
+    //récupération du token autorisé dans headers + split pour enlever bearer
+    const token = req.headers.authorization.split(' ')[1];
+    //décoder le token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    //récupération du userId du token décodé
+    const userId = decodedToken.userId;
+    //Ajout userId du token décodé dans requête d’authentification
+    //pour le cas ou pas de body dans la requête (ex route delete)
+    req.auth = { userId };
+    //vérifier si userId de la requête correspond au userId autorisé
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'User ID non valide!';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(403).json({ error: error | 'unauthorized request' });
+  }
+}; //end middleware authentification
